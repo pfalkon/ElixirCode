50ba4ace132b4fa8d89f715845512c33393e420e
Pawel Sokolowski <p.sokolowski@samsung.com>
2019-01-24 22:06:06 +0100

    Add Topic controller, model, view, routes, and create form
    Change-Id: I3188a88691ca2d0c3bf45956d47da054fe793d7f

---
+++ discuss/web/controllers/topic_controller.ex
@@ -0,0 +1,16 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def new(conn, _params) do
+    changeset = Topic.changset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+
+    conn
+  end
+end
