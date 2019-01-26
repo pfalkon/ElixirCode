fa2e977125f46ce178446530ca67243043b5da7c
Pawel Sokolowski <p.sokolowski@samsung.com>
2019-01-26 20:43:15 +0100

    Setup deps & database for this branch

--- discuss/config/dev.exs
+++ discuss/config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "stephengrider",
-  password: "",
+  username: "phoenix",
+  password: "phoenix#456",
   database: "discuss_dev",
   hostname: "localhost",
   pool_size: 10
